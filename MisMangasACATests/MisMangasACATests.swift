6c5c37f930acb1c34d71e4606cf99954921da9b1
Juan Ignacio Antolini <j.antolini@me.com>
2025-06-19 13:38:56 -0300

    Initial Commit

---
+++ MisMangasACATests/MisMangasACATests.swift
@@ -0,0 +1,17 @@
+//
+//  MisMangasACATests.swift
+//  MisMangasACATests
+//
+//  Created by Juan Ignacio Antolini on 19/06/2025.
+//
+
+import Testing
+@testable import MisMangasACA
+
+struct MisMangasACATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
